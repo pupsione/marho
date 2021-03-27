f993a8ae57c028cf869046450c694a3e245eee65
pupsione <delait.tryz000@gmail.com>
2021-03-27 19:59:50 +0100

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,19 @@
-$(function(){
+$(function () {
 
     $('.top-slider__inner').slick({
-        dots:true,
+        dots: true,
         arrows: false,
         fade: true,
-        autoplay:true,
+        autoplay: true,
         autoplaySpeed: 2000
+    });
+
+    $(".star").rateYo({
+        rating: 3.6,
+        starWidth: "17px",
+        normalFill: "#ccccce",
+        ratedFill: "#ffc35b",
+        readOnly: true
     });
 
 });
